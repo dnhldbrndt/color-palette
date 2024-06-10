b5269c381b34f53f7d01708695fe887e188f7239
dnhldbrndt <dnlhldbrndt@gmail.com>
2024-06-10 19:04:33 +0200

    Update home.html, app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import os
+ 
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from PIL import Image
@@ -28,7 +29,11 @@ def get_colors (filename):
                 color_dict[t_rgb] = 0
             if t_rgb in color_dict: 
                 color_dict[t_rgb] += 1
-    print(color_dict)
+    # print(color_dict)
+    sorted_colors = dict(sorted(color_dict.items(), key=lambda x: x[1], reverse=True))
+    color_list = list(sorted_colors.keys())
+    color_list = color_list[0:10]
+    print(color_list)
     return color_list
 
 # Convert RGB to Hex
@@ -39,8 +44,18 @@ def rgb_to_hex(rgb):
 @app.route('/', methods=['GET', 'POST'])
 def go_home():
     if request.method == 'POST':
-        img = upload_file()
-        return render_template('home.html', img=img)
+        tuple_img = upload_file()
+        img = tuple_img[0]
+        filename = tuple_img[1]
+        colors_list = get_colors(filename)
+        hex_list = []
+        render_colors = []
+        for color in colors_list:
+            hex_value = rgb_to_hex(color)
+            tmp_color = {'rgb': color, 'hex':hex_value}
+            hex_list.append(hex_value) 
+            render_colors.append(tmp_color)
+        return render_template('home.html', img=img, colors_list=colors_list, render_colors=render_colors, hex_list=hex_list )
     return render_template('home.html')
 
 
@@ -49,9 +64,8 @@ def upload_file():
     file = request.files['img']
     filename = secure_filename(file.filename)
     file.save(os.path.join(app.config['UPLOAD'], filename))
-    img = os.path.join(app.config['UPLOAD'], filename)
-    get_colors(filename)
-    return img
+    img = os.path.join(app.config['UPLOAD'], filename)  
+    return tuple([img, filename])
    
    
 # Run the Flask app
